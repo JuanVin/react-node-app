c2424f3a0cc592fce3397bf1b482d34398e4cc92
Juan <jm_vincii@hotmail.com>
2022-05-06 11:51:00 -0300

    .

--- frontend/src/components/CellTable.jsx
+++ frontend/src/components/CellTable.jsx
@@ -10,12 +10,13 @@ function CellTable(files) {
                 <td>{row.File.file_number}</td>
                 <td>{row.shift_date}</td>
                 <td>{row.admission_date}</td>
+                <td><button type="button" className="btn btn-primary">Ver detalle</button></td>
             </tr>
         )
     })
 
     return (
-        <><Table striped bordered hover>
+        <><Table striped bordered hover className="mt-5">
             <thead>
                 <tr>
                     <th>Id Exp</th>
