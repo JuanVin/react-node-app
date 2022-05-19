e641cddf0df7f43585dcf1851db72b92ccc514d5
Juan Vinci <jm_vincii@hotmail.com>
2022-05-19 20:22:40 -0300

    .

--- frontend/src/components/DeviceForm.jsx
+++ frontend/src/components/DeviceForm.jsx
@@ -1,4 +1,4 @@
-import { AiOutlineCheck, AiOutlineCloseCircle } from "react-icons/ai";
+import { AiOutlineCheck, AiOutlineCloseCircle, AiOutlineWarning } from "react-icons/ai";
 
 function DeviceForm() {
     return (
@@ -28,31 +28,33 @@ function DeviceForm() {
                 <>
                     <div className="w-100 p-1">
                         <div className="form-group">
-                            <h3 style={{color: "grey", marginLeft: "15px"}}>Física</h3>
-                            <table class="table">
+                            <h3 style={{ color: "grey", marginLeft: "15px" }}>Física</h3>
+                            <table class="table table-sm">
                                 <thead>
                                     <tr className="text-center">
-                                        <th scope="col">Decrypted Boot Loader </th>
-                                        <th scope="col">Boot Loader</th>
-                                        <th scope="col">ADB</th>
-                                        <th scope="col">EDL</th>
-                                        <th scope="col">Qualcomm</th>
+                                        <th scope="col" style={{maxWidth: "100px" }}>Decrypted Boot Loader </th>
+                                        <th scope="col" style={{maxWidth: "100px" }}>Boot Loader</th>
+                                        <th scope="col" style={{maxWidth: "100px" }}>ADB</th>
+                                        <th scope="col" style={{maxWidth: "100px" }}>EDL</th>
+                                        <th scope="col" style={{maxWidth: "100px" }}>Qualcomm</th>
+                                        <th scope="col" style={{maxWidth: "100px" }}>Advanced ADB</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
-                                        <td className="text-center" style={{color: "rgb(51, 160, 51)", fontSize: "35px"}}><AiOutlineCheck/></td>
-                                        <td></td>
-                                        <td></td>
-                                        <td></td>
-                                        <td></td>
+                                    <tr className="text-center">
+                                        <td className="text-center" style={{ color: "rgb(51, 160, 51)", fontSize: "35px", maxWidth: "150px" }}><AiOutlineCheck /></td>
+                                        <td className="text-center" style={{ color: "grey", fontSize: "20px" }}></td>
+                                        <td className="text-center" style={{ color: "grey", fontSize: "20px" }}></td>
+                                        <td className="text-center" style={{ color: "grey", fontSize: "20px" }}></td>
+                                        <td className="text-center" style={{ color: "rgb(180, 180, 0)", fontSize: "35px" }}> <AiOutlineWarning/></td>
+                                        <td className="text-center" style={{ color: "grey", fontSize: "20px" }}></td>
                                     </tr>
                                 </tbody>
                             </table>
                         </div>
 
                         <div className="form-group">
-                            <h3 style={{color: "grey", marginLeft: "15px"}}>Lógica</h3>
+                            <h3 style={{ color: "grey", marginLeft: "15px" }}>Lógica</h3>
                             <table class="table">
                                 <thead>
                                     <tr className="text-center">
@@ -66,17 +68,16 @@ function DeviceForm() {
                                 <tbody>
                                     <tr className="text-center">
                                         <td></td>
-                                        <td style={{color: "rgb(51, 160, 51)", fontSize: "35px"}}><AiOutlineCheck/></td>
+                                        <td style={{ color: "rgb(51, 160, 51)", fontSize: "35px" }}><AiOutlineCheck /></td>
                                         <td></td>
-                                        <td><img style={{ width: "40px" }} src="https://www.interweave.in/wp-content/themes/interweave-theme/images/fail.png"></img></td>
+                                        <td style={{ fontSize: "40px", color: "rgb(241, 83, 83)" }}><AiOutlineCloseCircle /></td>
                                         <td></td>
                                     </tr>
                                 </tbody>
                             </table>
                         </div>
-
                         <div className="form-group">
-                            <h3 style={{color: "grey", marginLeft: "15px"}}>Sistema de archivos</h3>
+                            <h3 style={{ color: "grey", marginLeft: "15px" }}>Sistema de archivos</h3>
                             <table class="table">
                                 <thead>
                                     <tr className="text-center">
@@ -91,48 +92,51 @@ function DeviceForm() {
                                     <tr className="text-center">
                                         <td></td>
                                         <td></td>
-                                        <td style={{color: "rgb(51, 160, 51)", fontSize: "35px"}}><AiOutlineCheck/></td>
-                                        <td style={{color: "rgb(51, 160, 51)", fontSize: "35px"}}><AiOutlineCheck/></td>
+                                        <td style={{ color: "rgb(51, 160, 51)", fontSize: "35px" }}><AiOutlineCheck /></td>
+                                        <td style={{ color: "rgb(51, 160, 51)", fontSize: "35px" }}><AiOutlineCheck /></td>
                                     </tr>
                                 </tbody>
                             </table>
                         </div>
                         <div className="form-group">
-                            <h3 style={{color: "grey", marginLeft: "15px"}}>Simcard</h3>
-                            <table class="table">
-                                <thead>
-                                    <tr className="text-center">
-                                        <th scope="col">Volcado</th>
-                                        <th scope="col">No posee</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr className="text-center">
-                                        <td style={{fontSize: "40px", color: "rgb(241, 83, 83)"}}><AiOutlineCloseCircle/></td>
-                                        <td><img style={{ width: "40px" }} src=""></img></td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                        <div className="form-group">
-                            <h3 style={{color: "grey", marginLeft: "15px"}}>MicroSD</h3>
-                            <table class="table">
-                                <thead>
-                                    <tr className="text-center">
-                                        <th scope="col">Volcado</th>
-                                        <th scope="col">No posee</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr className="text-center">
-                                        <td><img style={{ width: "40px" }} src=""></img></td>
-                                        <td style={{color: "rgb(51, 160, 51)", fontSize: "35px"}}><AiOutlineCheck/></td>
-                                    </tr>
-                                </tbody>
-                            </table>
+                            <div className="row">
+                                <div className="col">
+                                    <h3 style={{ color: "grey", marginLeft: "15px" }}>Simcard</h3>
+                                    <table class="table">
+                                        <thead>
+                                            <tr className="text-center">
+                                                <th scope="col">Volcado</th>
+                                                <th scope="col">No posee</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr className="text-center">
+                                                <td style={{ fontSize: "40px", color: "rgb(241, 83, 83)" }}><AiOutlineCloseCircle /></td>
+                                                <td><img style={{ width: "40px" }} src=""></img></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div className="col">
+                                    <h3 style={{ color: "grey", marginLeft: "15px" }}>MicroSD</h3>
+                                    <table class="table">
+                                        <thead>
+                                            <tr className="text-center">
+                                                <th scope="col">Volcado</th>
+                                                <th scope="col">No posee</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr className="text-center">
+                                                <td><img style={{ width: "40px" }} src=""></img></td>
+                                                <td style={{ color: "rgb(51, 160, 51)", fontSize: "35px" }}><AiOutlineCheck /></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
                         </div>
                     </div>
-
                 </>
             }
         </>
